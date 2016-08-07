3ec707a078bc239efd20ef8cca3ecb1f00917a4d
czbly <641571818@qq.com>
2016-08-08 01:18:07 +0800

    banner

--- zlqy/js/index.js
+++ zlqy/js/index.js
@@ -43,8 +43,8 @@ function big(value) {//放大函数
     var width = parseInt(inner.style.width);
     var height = parseInt(inner.style.height);
     if(value != null) {
-        inner.style.width = width *1.01 + 'px'; 
-        inner.style.height = height*1.01 + 'px';
+        inner.style.width = width *1.02 + 'px'; 
+        inner.style.height = height*1.02 + 'px';
     }
 }
 
@@ -52,8 +52,8 @@ function small(value) {//缩小函数
     var width = parseInt(inner.style.width);
     var height = parseInt(inner.style.height);
     if(value != null) {
-        inner.style.width = width /1.01 + 'px'; 
-        inner.style.height = height/1.01 + 'px';
+        inner.style.width = width /1.02 + 'px'; 
+        inner.style.height = height/1.02 + 'px';
     }
 }
 
@@ -75,7 +75,7 @@ jian.onclick = function () {
     num--;
 }
 
-    flag = 0;
+var flag = 0;
 var pic = document.querySelectorAll('.same');
 var index = pic.length;
 var picon = document.getElementById("picCon");
@@ -84,32 +84,37 @@ var time = setInterval("animate();",3000);
 
 
 
-
+picwhole.addEventListener('mouseover',function() {
+    clearInterval(time);
+})
+picwhole.addEventListener('mouseout',function() {
+    time = setInterval("animate();",3000);
+})
 
 
 
 picon.addEventListener('mouseover',function() {
     pre.style.display = 'block';
     next.style.display = 'block';
-    clearInterval(time);
+    // clearInterval(time);
 })
 picon.addEventListener('mouseout',function() {
     pre.style.display = 'none';
     next.style.display = 'none';
-    time = setInterval("animate();",3000);
+    // time = setInterval("animate();",3000);
 })
 
 pre.addEventListener('mouseover', function() {
-    clearInterval(time);
+    // clearInterval(time);
 })
 pre.addEventListener('mouseout', function() {
-    time = setInterval("animate();",3000);
+    // time = setInterval("animate();",3000);
 })
 next.addEventListener('mouseover', function() {
-    clearInterval(time);
+    // clearInterval(time);
 })
 next.addEventListener('mouseout', function() {
-    time = setInterval("animate();",3000);
+    // time = setInterval("animate();",3000);
 })
 function animate(val) {//轮播函数
     if(val != null) {
@@ -128,31 +133,27 @@ function animate1(valu) {
     if(valu != null) {
         flag = val - 2;
     }
-    if(flag < index - 1) {
+    if(0 < flag < index - 1) {
         flag--; 
     }
     else {
         flag = 0;
     }
-    if(flag = 0) {
-        slider.style.left = 0 + 'px'; 
-    }
-    else{
-        slider.style.left = flag * -465 + 'px';
-    }
+    slider.style.left = flag * -465 + 'px';
     shown(flag);
 }
 
 
 
 next.onclick = function () { 
-    clearInterval(time);
+    // clearInterval(time);
    animate();
 }
 
 pre.onclick = function () {
-    clearInterval(time);
+    // clearInterval(time);
    animate1();
+
 }
     
 taiji.onmouseover = function () {
@@ -239,98 +240,98 @@ chqs.onmouseover = function () {
 
 taiji.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(9);
     shown(8);
 }
 
 laocao.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(15);
     shown(14);
 }
 oldlib.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(1);
     shown(0);
 }
 bashiwan.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(3);
     shown(2);
 }
 qrp.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(7);
     shown(6);
 }
 xk.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(6);
     shown(5);
 }
 fengyu.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(5);
     shown(4);
 }
 wudong.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(8);
     shown(7);
 }
 shutu.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(16);
     shown(15);
 }
 yf.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(10);
     shown(9);
 }
 newgate.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(11);
     shown(10);
 }
 erjiao.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(4);
     shown(3);
 }
 sijiao.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(14);
     shown(13);
 }
 sanjiao.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(13);
     shown(12);
 }
 bajiao.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(2);
     shown(1);    
 }
 chqs.onclick = function () {
     tanchuang.style.top = 100 + 'px';
-    clearInterval(time);
+    // clearInterval(time);
     animate(12);
     shown(11);
 }
@@ -355,10 +356,10 @@ function shown (some) {
         case 9 :biaoti.innerHTML = "逸夫科技楼" , pa.innerHTML = "这里有着其他地方无可比拟的厚度，一个甲子的沉淀，使得历史的气息从一进门便扑面而来，校史馆的每一件展品都藏着一个老故事，只要你看过，就有动力为重邮未来添上一笔。"; break;
         case 10 :biaoti.innerHTML = "新校门" , pa.innerHTML = "谈不上气派，可就是有一种说不清的韵味。一枚小小的邮票，任邮戳在湛蓝的天空下飞舞。站在门前，享受着微风抚摸脸颊，淡淡的书香，吹进耳朵，变成了闲时嬉戏打闹的声音，不一会儿，就成了运动时的呐喊了。"; break;
         case 11 :biaoti.innerHTML = "春华秋实广场" , pa.innerHTML = "当黄桷馨声与草坪音乐的歌声在这里响起，当辛勤的学子从这里经过走进老图，奔向信科...她在这里见证了一代又一代邮子的成长，见证了 一个又一个的成功。"; break;
-        case 12 :biaoti.innerHTML = "三教" , pa.innerHTML = "国旗飘扬在前，雏鸟啼鸣在侧，鸟语花香，读书之地，波光潋滟，奋斗之初，这里有“三国”，有学子，有故事，最自豪的，便是在清晨看着缓缓升起的国旗，耳边不断传送着国歌。"; break;
-        case 13 :biaoti.innerHTML = "四教" , pa.innerHTML = "大一的时候，这里有咱们重邮人每周必去的“英语一条街”。这栋拥有着年代感的第四教学楼，也是我们梦想的聚集地。"; break;
-        case 14 :biaoti.innerHTML = "老操" , pa.innerHTML = "不管秋冬还是春夏，不管刮风还是下雨，不管日升还是日落，重邮人总是奔跑在这儿，坚信自己的梦想风向标就在前方。"; break;
-        case 15 :biaoti.innerHTML = "数图" , pa.innerHTML = "在这里，有最全面的数字信息，有最前端的科技资讯，有最开阔的“互联网+”平台。"; break;
+        case 12 :biaoti.innerHTML = "三教(图片来自网络)" , pa.innerHTML = "国旗飘扬在前，雏鸟啼鸣在侧，鸟语花香，读书之地，波光潋滟，奋斗之初，这里有“三国”，有学子，有故事，最自豪的，便是在清晨看着缓缓升起的国旗，耳边不断传送着国歌。"; break;
+        case 13 :biaoti.innerHTML = "四教(图片来自网络)" , pa.innerHTML = "大一的时候，这里有咱们重邮人每周必去的“英语一条街”。这栋拥有着年代感的第四教学楼，也是我们梦想的聚集地。"; break;
+        case 14 :biaoti.innerHTML = "老操(图片来自网络)" , pa.innerHTML = "不管秋冬还是春夏，不管刮风还是下雨，不管日升还是日落，重邮人总是奔跑在这儿，坚信自己的梦想风向标就在前方。"; break;
+        case 15 :biaoti.innerHTML = "数图(图片来自网络)" , pa.innerHTML = "在这里，有最全面的数字信息，有最前端的科技资讯，有最开阔的“互联网+”平台。"; break;
     }
 
 }
